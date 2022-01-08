49d942b3cfa65a9db73798efeda85e7a876b4f4f
Daleyar <ali.daley72@gmail.com>
2022-01-08 16:54:12 -0500

    Created and tested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Rental from './components/Rental';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/rental" component={Rental} />
+        </Switch>
+      </div>
+      </Router>
   );
 }
 
